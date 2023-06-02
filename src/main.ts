3d6591b75ae215123968d5ac1d03d93a82462ae4
William Antoniazzi <william.antoniazzi@fatec.sp.gov.br>
2023-06-02 23:22:03 +0000

    Updated

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://8080-williamanton-provalabbd-h8q0gpixs7u.ws-us97.gitpod.io';
+axios.defaults.baseURL = 'https://8080-williamanton-provalabbd-h8q0gpixs7u.ws-us98.gitpod.io';
 
 import './assets/main.css'
 
